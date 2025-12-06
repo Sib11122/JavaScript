f2ebf1300a96cdfe8cbc4e630e094d054daac261
Sehaj deep <Dsahej2004@outlook.com>
2025-12-06 21:50:45 +0530

    ok

--- Arrays/scripts.js
+++ Arrays/scripts.js
@@ -1,24 +1,27 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+// const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+// const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+// const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
-// Breakfast menu
-let breakfastItem = '';
-for (let a = 0; a < breakfastMenu.length; a++) {
-    breakfastItem += `<p>Item ${a + 1}: ${breakfastMenu[a]}</p>`;
-}
-document.getElementById('breakfastMenuItems').innerHTML = breakfastItem;
+// // Breakfast menu
+// let breakfastItem = '';
+// for (let a = 0; a < breakfastMenu.length; a++) {
+//     breakfastItem += `<p>Item ${a + 1}: ${breakfastMenu[a]}</p>`;
+// }
+// document.getElementById('breakfastMenuItems').innerHTML = breakfastItem;
 
-// Main course menu
-let mainCourseItem = '';
-mainCourseMenu.forEach((item, index) => {
-    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
-});
-document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+// // Main course menu
+// let mainCourseItem = '';
+// mainCourseMenu.forEach((item, index) => {
+//     mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
+// });
+// document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
 
-// Dessert menu
-let dessertItem = '';
-for (let i = 0; i < dessertMenu.length; i++) {
-    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
-}
-document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+// // Dessert menu
+// let dessertItem = '';
+// for (let i = 0; i < dessertMenu.length; i++) {
+//     dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
+// }
+// document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+let arr=[22,23,21];
+let a=arr.shift(arr);
+console.log(a);
